26127ff097fd051a6c936d9557e9a4048a0cd3c3
Ian Gordon <gordonianjames@icloud.com>
2021-08-04 09:06:07 -0700

    Initial commit

---
+++ cmd/root.go
@@ -0,0 +1,80 @@
+/*
+Copyright © 2021 Ian James Gordon
+Copyrights apply to this source code.
+Check LICENSE for details.
+
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "seccloud",
+	Short: "Secure Cloud Creator",
+	Long: `seccloud - short for Secure Cloud - stands up a secure 
+cloud presence.
+
+seccloud allows you to stand up a suite of applications in a zero trust
+security architecture at a cloud provider of your choice.`,
+	Version: "0.1.0",
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	cobra.CheckErr(rootCmd.Execute())
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.seccloud.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	versionTemplate := `{{printf "%s: %s - version %s\n" .Name .Short .Version}}`
+	rootCmd.SetVersionTemplate(versionTemplate)
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".seccloud" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".seccloud")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
---
+++ main.go
@@ -0,0 +1,13 @@
+/*
+Copyright © 2021 Ian James Gordon
+Copyrights apply to this source code.
+Check LICENSE for details.
+
+*/
+package main
+
+import "github.com/gordonianj/seccloud/cmd"
+
+func main() {
+	cmd.Execute()
+}
